a172820727f96dc708bdc2fc59184778332ca582
caua2512 <caua.h@escolar.ifrn.edu.br>
2025-05-12 23:37:38 -0300

    processo em andamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,51 @@
-import Image from "next/image";
+const LoginModal = () => {
+    return (
+        <>
+            <body>
+                <header className="bg-blue-500">
+                    <h2>
+                        <img src="@/imagens/person_filled_FILL0_wght400_GRAD0_opsz48 1.png"/>
+                    </h2>
+                    <figure>
+                        <img 
+                            src="@/imagens/menu-sanduiche.png"
+                            alt="botão de Menu"
+                            width={100}
+                            height={100}
+                        />
+                    </figure>
+                </header>
+                <main className="bg-white w-full h-full">
+                    <h1 className="text-blue-500 text-2xl flex justify-center">Cadastra-se</h1>
+                    <form className="flex align-center justify-center flex-col">
+                        <div className="flex justify-center flex-col gap-1.5">
+                            <span className="text-blue-500">Cpf</span>  
+                            <div className="flex flex-row border-b-3 border-gray-200 w-70 gap-x-6">
+                                <figure><img src="@/app/Vector.png"/></figure>
+                                <input
+                                    type="text"
+                                    placeholder="Digite seu cpf"
+                                    className="w-70 h-8 text-gray-800"
+                                />
+                            </div>
+                            <span className="text-blue-500">Senha</span>
+                            <div className="flex flex-row border-b-3 border-gray-200 w-70 gap-x-6">
+                                <figure><img src="@/app/lock_FILL0_wght400_GRAD0_opsz48 1.png"/></figure>
+                                <input
+                                    type="password"
+                                    placeholder="Digite sua senha"
+                                    className="w-70 h-8 text-gray-800"
+                                />
+                            </div>
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+                            <button className="w-75 h-10 text-white bg-blue-500 rounded-4xl">Cadastra-se</button>
+                        </div>
+                    </form>
+                    <p className="text-gray-400">se já possuir uma conta faça login <b className="text-black">aqui</b></p>
+                </main>
+            </body>
+        </>
+    )
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+export default LoginModal ;
