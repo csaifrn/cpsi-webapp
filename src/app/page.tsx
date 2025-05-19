6737104b45cc09fa6d8032471736698a1f656ddc
caua2512 <caua.h@escolar.ifrn.edu.br>
2025-05-19 19:45:30 -0300

    mini ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-white h-screen">
+        <div className="flex flex-col items-center border-2 border-blue-400 rounded-lg m-4">
+            <h3 className="text-black text-sm font-bold self-start border-b-2 border-blue-400 p-3 pl-4 w-full">Documentação</h3>
+            <section className="flex flex-col justify-center items-center mt-4 mb-4">
+                 <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-t-2 border-l-2 border-r-2 p-3">FOTO 3x4</p>
+                </div>
+                <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-t-2 border-l-2 border-r-2 p-3">CÓPIA DO RG (FRENTE)</p>
+                </div>
+                 <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-t-2 border-l-2 border-r-2 p-3">CÓPIA DO RG (VERSO)</p>
+                </div>
+                <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-t-2 border-l-2 border-r-2 p-2.5">COMPROVANTE DE RESIDÊNCIA</p>
+                </div>
+                 <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-t-2 border-l-2 border-r-2 p-2.5">PASSAPORTE VACINAL</p>
+                </div>
+                <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-t-2 border-l-2 border-r-2 p-2.5">ATESTADO CARDIOLOGISTA</p>
+                </div>
+                 <div className="flex row-auto">
+                    <figure className="border-blue-400 border-t-2 border-l-2 border-b-2 p-3"><img height="20px" width="20px" src="/Check.svg"/></figure>
+                    <p className="text-black font-bold text-xs w-60 flex items-center border-blue-400 border-2 p-2.5">ATESTADO DERMATOLOGISTA</p>
+                </div>
+            </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
