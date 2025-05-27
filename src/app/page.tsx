d056240e3ede93e6635e7e700441aa9a16838aa5
caua2512 <caua.h@escolar.ifrn.edu.br>
2025-05-27 14:02:33 -0300

    oi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,53 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 const Documentacao = () => {
   return (
     <div className="bg-white h-screen">
+        <section className="flex flex-col items-center border-2 w-80 border-blue-400 rounded-lg m-4">
+            <h3 className="text-black text-sm font-bold self-start border-b-2 border-blue-400 p-3 pl-4 w-full">Documentação</h3>
+            <div className="flex flex-col justify-center items-center mt-4 mb-4">
+                <Table>
+                <TableBody className="border-2 border-blue-400">
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2"><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">FOTO 3x4</TableCell>
+                    </TableRow>
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2"><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">CÓPIA DO RG(FRENTE)</TableCell>
+                    </TableRow>
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2 "><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">CÓPIA DO RG (VERSO)</TableCell>
+                    </TableRow>
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2"><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">COMPROVANTE DE RESIDÊNCIA</TableCell>
+                    </TableRow>
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2"><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">PASSAPORTE VACINAL</TableCell>
+                    </TableRow>
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2 "><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">ATESTADO CARDIOLOGISTA</TableCell>
+                    </TableRow>
+                    <TableRow className="border-2 border-blue-400">
+                        <TableCell><figure className="border-blue-400 p-2"><img height="20px" width="20px" src="/Check.svg"/></figure></TableCell>
+                        <TableCell className="font-bold text-xs border-blue-400 border-l-2">ATESTADO DERMATOLOGISTA</TableCell>
+                    </TableRow>
+                </TableBody>
+                </Table>
+            </div>
+        </section>
         <section className="flex flex-col items-center border-2 w-80 border-blue-400 rounded-lg m-4">
             <h3 className="text-black text-sm font-bold self-start border-b-2 border-blue-400 p-3 pl-4 w-full">Documentação</h3>
             <div className="flex flex-col justify-center items-center mt-4 mb-4">
