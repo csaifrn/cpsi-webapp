ac97b31ca6bab0c2ce38f3d4e9559a857ccc9336
Maria Eduarda <eduarda.nascimento2@escolar.ifrn.edu.br>
2025-06-16 15:46:54 -0300

    atualizações

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { SquareUser, Menu } from 'lucide-react';
 const Head = (props) => {
   const visivel = props.visivel ? !props.visivel : false;
     return (
-      <header className="bg-indigo-500 text-white h-20" hidden={visivel}>
+      <header className="bg-principal-blue text-white h-20" hidden={visivel}>
         <ol className="flex flex-row justify-around gap-80">
           <li><a href="#"><SquareUser className=" h-10 w-10 mt-5"/></a></li>
           <li><a href="#"><Menu className=" h-10 w-10 mt-5"/></a></li>
