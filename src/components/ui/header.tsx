947fdbde8a6c805cece7d098d660513c45e320e5
Maria Eduarda <130762202+Duarda19@users.noreply.github.com>
2025-06-16 10:28:48 -0300

    Create header.tsx

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,15 @@
+import { SquareUser, Menu } from 'lucide-react';
+
+
+const Head = (props) => {
+  const visivel = props.visivel ? !props.visivel : false;
+    return (
+      <header className="bg-indigo-500 text-white h-20" hidden={visivel}>
+        <ol className="flex flex-row justify-around gap-80">
+          <li><a href="#"><SquareUser className=" h-10 w-10 mt-5"/></a></li>
+          <li><a href="#"><Menu className=" h-10 w-10 mt-5"/></a></li>
+        </ol>
+      </header>
+    );
+}
+export { Head };
