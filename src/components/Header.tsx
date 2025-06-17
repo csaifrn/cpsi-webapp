0f5e38eed2181df81ab31e01fe20d57d6264e158
Leonardo Minora <leonardo.minora@gmail.com>
2025-06-17 16:40:08 -0300

    adicionado header do app web

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Button } from "./ui/button";
+
+const Header = (props) => {
+  const visivel = props.visivel ? !props.visivel : false;
+
+	return (
+		<header className="bg-blue-500 flex itens-center align-center justify-between py-4 px-5 " hidden={visivel}>
+			<h2>
+				<img src="/perfil-transparente.svg" />
+			</h2>
+			<figure>
+				<Button className="bg-transparent border-0">
+					<img src="/menu-sanduiche.svg" alt="botão de Menu " />
+				</Button>
+			</figure>
+		</header>
+	);
+};
+
+export default Header;
