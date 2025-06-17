9ba6dac7f93e86fd5dac75f511176b85693addd9
Izabel <izabel.dantas@escolar.ifrn.edu.br>
2025-06-17 16:05:27 -0300

    criação de uma pagina secundaria para testar o header

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,12 @@
+import react from "react";
+import { Head } from "@/components/head";
+
+const Teste = () => {
+    return (
+        <div className="flex flex-col">
+            teste
+        </div>
+    );
+};
+
+export default Teste;
