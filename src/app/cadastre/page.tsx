618b9eba186e55b4efdab37c5dd1c4675a4951d6
Maria Eduarda <eduarda.nascimento2@escolar.ifrn.edu.br>
2025-06-21 16:05:34 -0300

    alterações na página de cadastro

--- src/app/cadastre/page.tsx
+++ src/app/cadastre/page.tsx
@@ -1,44 +1,54 @@
 // // app/page.tsx
 // import { Form } from "@/components/form";
 // import { Footer } from "@/components/footer";
-
+import { Poppins, ZCOOL_KuaiLe } from "next/font/google";
+import "../globals.css";
 import { User, LockKeyhole } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+});
+
 
 const Form = () => {
 	return (
-		<div className="text-principal-blue flex flex-col items-center justify-center min-h-screen font-mono">
+		<div className="text-blue-500 flex flex-col items-center {poppins.className}">
 			<h1 className="m-12 text-2xl text-center">CADASTRAR-SE</h1>
-			<form className="formulario">
-				<Label htmlFor="cpf" className="text-lg">
-					CPF
-				</Label>
-				<br />
-				<div className="flex flex-row m-3 mb-5 mt-0 ml-0 gap-3 border-b border-gray-400 py-2">
-					<User className="text-neutral-400 w-8 h-8" />
-					<Input
-						className="text-neutral-500 border-0"
-						type="cpf"
-						id="cpf"
-						placeholder="Digite seu CPF"
-					/>
+			<form className="formulario flex flex-col">
+				<div  className="mb-8">
+					<Label htmlFor="cpf" className="text-base">
+						CPF
+					</Label>
+					<br />
+					<div className="flex flex-row mr-3 mb-5 gap-0 border-b border-gray-400 ">
+						<User className="text-neutral-400 w-6 h-6 mt-1.5" />
+						<Input
+							className="text-neutral-500 border-0"
+							type="cpf"
+							id="cpf"
+							placeholder="Digite seu CPF"
+						/>
+					</div>
 				</div>
-				<Label htmlFor="email" className="text-lg">
-					Email
-				</Label>
-				<br />
-				<div className="flex flex-row m-3 mt-0 mb-5 ml-0 gap-3 border-b border-gray-400 py-2">
-					<LockKeyhole className="text-neutral-400 w-8 h-8" />
-					<Input
-						className="text-neutral-500 border-0"
-						type="password"
-						id="senha"
-						placeholder="Digite sua senha"
-					/>
+				<div className="mb-8">
+					<Label htmlFor="email" className="text-base">
+						Email
+					</Label>
+					<br />
+					<div className="flex flex-row mr-3 mb-5 gap-0 border-b border-gray-400 ">
+						<LockKeyhole className="text-neutral-400 w-6 h-6 mt-1.5" />
+						<Input
+							className="text-neutral-500 border-0"
+							type="password"
+							id="senha"
+							placeholder="Digite sua senha"
+						/>
+					</div>
 				</div>
-				<Button className="bg-principal-blue hover:bg-principal-blue focus:ring-0 text-white w-90 rounded-3xl h-12 mt-20">
+				<Button className="bg-blue-500 focus:ring-0 text-white w-90 rounded-3xl h-12 mt-20">
 					CADASTRE-SE
 				</Button>
 			</form>
