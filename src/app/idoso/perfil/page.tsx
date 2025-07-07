7989394ef2a7967909bf3b5bd7ca906710392747
Izabel <izabel.dantas@escolar.ifrn.edu.br>
2025-07-06 23:03:03 -0300

    criada a nav bar do idoso logado

--- src/app/idoso/perfil/page.tsx
+++ src/app/idoso/perfil/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { CardIdosoPerfilAtividades, CardDadosPessoais, CardDocumentacao } from "@/components/idoso";
+import { CardIdosoPerfilAtividades, CardDadosPessoais, CardDocumentacao, NavBarIdoso } from "@/components/idoso";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Car } from "lucide-react";
 
+
 export default function page() {
 	return (
 		<section className="min-h-screen bg-white">
-
+			<NavBarIdoso />
 			<div className="py-4 max-w-md mx-auto text-gray-900 font-courier-prime">
 				<div className="flex items-center text-lg font-semibold">
 					<Button variant="ghost" size="icon" className="mr-2 p-0 h-auto w-auto">
