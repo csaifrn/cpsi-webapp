88432d65763c59753a877770d8ef4ae09781c4c7
Izabel <izabel.dantas@escolar.ifrn.edu.br>
2025-07-28 17:19:42 -0300

    Alterando link para login

--- src/app/cadastre/page.tsx
+++ src/app/cadastre/page.tsx
@@ -1,13 +1,15 @@
 
 import Form from "@/components/cadastro/cadastro-form";
-
+import Link from "next/link";
 
 const Footer = () => {
 	return (
 		<footer className="mt-20 mb-20 text-center font-mono">
 			<p>
 				Se já possuir uma conta <br /> faça login{" "}
-				<span className="text-gray-700 font-bold">aqui</span>
+				<Link href="/">
+					<span className="text-gray-700 font-bold">aqui</span>
+				</Link>
 			</p>
 		</footer>
 	);
