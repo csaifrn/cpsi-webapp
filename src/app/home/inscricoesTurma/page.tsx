8ff649e9e112aae6defabfecb919f356e29aef34
caua2512 <caua.h@escolar.ifrn.edu.br>
2025-08-20 14:46:48 -0300

    Finalizado

--- src/app/home/inscricoesTurma/page.tsx
+++ src/app/home/inscricoesTurma/page.tsx
@@ -4,6 +4,7 @@ import { Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { ArrowLeft, AlertCircle, ChevronRight, Search } from "lucide-react";
 import Header from "@/components/Header";
+import { CardHome } from "@/components/home";
 
 const pessoas = [
   {
@@ -61,22 +62,22 @@ const InscricoesPage = () => {
   return (
     <div className="flex flex-col font-courier-prime">
       <Header />
-      <div className="flex flex-col items-center justify-center mt-6 mb-4 w-90">
+      <CardHome>
         {/* Voltar */}
-        <div className="flex items-center mr-60 gap-2 py-2">
+        <div className="flex items-center mr-60 my-2 gap-2 py-2">
             <ArrowLeft className="w-5 h-5" />
             <span className="font-bold text-sm">Voltar</span>
         </div>
 
         {/* Card da turma */}
         <Card className="flex flex-row items-center w-80 h-24 gap-3 pr-3 rounded-lg shadow bg-primaria-yellow mb-2">
-            <img src="/imagens/turmas/icones/danca_nao_preenchido.svg" alt="Dança" className="w-15 h-15" />
+            <img src="/imagens/turmas/icones/danca_nao_preenchido.svg" alt="Dança" className="w-12 h-12 ml-3" />
             <div className="flex-1 flex-col gap-2">
             <div className="font-bold text-lg">Dança</div>
             <div className="text-xs font-bold">Turma 1</div>
             <div className="text-xs">Prof. Julio</div>
             </div>
-            <div className="text-xs text-right mb-10">12:35 - 14:00</div>
+            <div className="text-xs text-right mb-8.5">12:35 - 14:00</div>
         </Card>
 
         {/* Vagas e inscrições */}
@@ -95,7 +96,7 @@ const InscricoesPage = () => {
         </div>
 
         {/* Busca */}
-        <div className="mb-4 pl-3 flex justify-start items-center w-80 bg-white border-2 focus:border-none text-gray-800 text-sm rounded-full">
+        <div className="mb-2 pl-3 flex justify-start items-center w-80 bg-white border-2 focus:border-none text-gray-800 text-sm rounded-full">
           <Search className=""/>
           <Input
               type="text"
@@ -128,8 +129,8 @@ const InscricoesPage = () => {
                 <div>
                 {pessoa.alerta ? (
                   <div className="flex flex-col items-center gap-2">
-                    <AlertCircle className="w-4 h-4 text-red-500 mb-2" />
-                    <ChevronRight className="w-4 h-4 text-primaria-blue mb-9" />
+                    <AlertCircle className="w-3 h-3 text-red-500 mb-0.5" />
+                    <ChevronRight className="w-4 h-4 text-primaria-blue mb-4.5" />
                   </div>   
                 ) : (
                     <ChevronRight className="w-4 h-4 text-primaria-blue" />
@@ -138,7 +139,7 @@ const InscricoesPage = () => {
             </Card>
             ))}
         </Card>
-      </div>
+      </CardHome>
     </div>
 
     );
